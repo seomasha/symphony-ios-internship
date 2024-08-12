d2eae96a29051acc29325fc1a732bef77485a1b1
Denis-Tojaga <denis.tojaga@symphony.is>
2024-08-12 10:33:06 +0200

    Initializing app

---
+++ InternshipApp/InternshipApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InternshipApp
+//
+//  Created by Denis Tojaga on 12. 8. 2024..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InternshipApp/InternshipApp/InternshipAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternshipAppApp.swift
+//  InternshipApp
+//
+//  Created by Denis Tojaga on 12. 8. 2024..
+//
+
+import SwiftUI
+
+@main
+struct InternshipAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
