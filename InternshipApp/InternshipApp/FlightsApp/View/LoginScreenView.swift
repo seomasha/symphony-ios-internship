7f4ff806975158b856377adbf6bd262efd973c0a
seadmasetic <75685855+seomasha@users.noreply.github.com>
2024-08-19 11:37:03 +0200

    Created a splash screen for the FlightsApp

---
+++ InternshipApp/InternshipApp/FlightsApp/View/LoginScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreenView.swift
+//  InternshipApp
+//
+//  Created by Sead Mašetić on 19. 8. 2024..
+//
+
+import SwiftUI
+
+struct LoginScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginScreenView()
+}
